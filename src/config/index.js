000ae7542807ecd9a2c1c5551acadb5518c5eab8
Marvellous Ezekiel <ezekielmarvelly@gmail.com>
2026-03-27 20:50:50 +0100

    update prod url

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  BASE_URL: process.env.BASE_URL || 'https://mivax-cred.onrender.com', // fallback for current logic
+  BASE_URL: process.env.BASE_URL || 'https://mivax.marvelly.com.ng/', // fallback for current logic
   SIS_BASE_URL: 'https://sis-be.miva.university',
   SESSIONS_DIR: path.join(__dirname, '../../sessions'),
 };
