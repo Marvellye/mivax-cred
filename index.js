469feec69e73b7e8126c903e08662df57a4ab6a8
Marvellous Ezekiel <ezekielmarvelly@gmail.com>
2026-04-02 20:03:25 +0100

    add proxy trust for ip logging on analytics

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 const { PORT } = require('./src/config');
+app.set('trust proxy', true);
 
 app.listen(PORT, () => {
   console.log(`MivaX Server running on http://localhost:${PORT}`);
